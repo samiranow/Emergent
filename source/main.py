92b5089aa07ef19b1f397ca22c77c387e55d78a6
Shatak VPN <ShatakVPN@gmail.com>
2025-08-25 03:04:15 +0330

    Update main.py
    Add "local-config.txt" file to the code to use local V2RAY config if needed...

--- source/main.py
+++ source/main.py
@@ -12,6 +12,7 @@ import zoneinfo
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 URLS = [
+    "https://raw.githubusercontent.com/ShatakVPN/ConfigForge-V2Ray/refs/heads/main/source/local-config.txt",
     "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
     "https://raw.githubusercontent.com/HosseinKoofi/GO_V2rayCollector/main/mixed_iran.txt",
     "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt",
