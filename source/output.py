839fe7d6b4bbfc4681393ab82fa3329000a74a76
Shatak VPN <ShatakVPN@gmail.com>
2025-08-29 18:57:43 +0330

    Update output.py

--- source/output.py
+++ source/output.py
@@ -1,8 +1,6 @@
 import os
-from config import OUTPUT_DIR
 
-def save_to_file(filename: str, lines: list):
-    path = os.path.join(OUTPUT_DIR, filename)
+def save_to_file(path: str, lines: list):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w", encoding="utf-8") as f:
         f.write("\n".join(lines))
